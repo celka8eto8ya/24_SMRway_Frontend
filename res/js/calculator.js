f17dc7acf1736d8e3be768d7f6e7871a9da14d58
Andrei K. <kostychik@bk.ru>
2025-01-19 16:23:56 +0300

    Almost ready App. Added goal operations + refactoring, optimize code (functions)

--- res/js/calculator.js
+++ res/js/calculator.js
@@ -1,6 +1,13 @@
+// Remove last character
 document.getElementById("btn_delete").onclick = function () {
-  document.getElementById("screen").value =
-    document.getElementById("btn_delete").value;
+  screenArray = Array.from(document.getElementById("screen").value);
+  if (document.getElementById("screen").value.length == 1) {
+    document.getElementById("screen").value = "0";
+  }
+  if (document.getElementById("screen").value != "0") {
+    screenArray.pop();
+    document.getElementById("screen").value = screenArray.join("");
+  }
 };
 
 // Массив с идентификаторами кнопок
@@ -17,20 +24,6 @@ const buttonIds = [
   "btn_0",
 ];
 
-// adding button text to screen
-function printBtnText(btn) {
-  screenText = document.getElementById("screen").value;
-  if (screenText == "0") {
-    document.getElementById("screen").value = btn.value;
-  } else {
-    document.getElementById("screen").value += btn.value;
-  }
-}
-// Функция для обработки нажатия на кнопку
-function handleButtonClick(button) {
-  printBtn(button);
-}
-
 // Привязываем обработчик событий к каждой кнопке
 buttonIds.forEach((id) => {
   document.getElementById(id).onclick = function () {
@@ -38,7 +31,101 @@ buttonIds.forEach((id) => {
   };
 });
 
-// Привязываем обработчик событий к каждой кнопке
-buttonIds.forEach((id) => {
-  console.log(id);
+// Reverse sign
+document.getElementById("btn_plus-minus").onclick = function () {
+  if (document.getElementById("screen").value != "0") {
+    document.getElementById("screen").value =
+      +document.getElementById("screen").value * -1;
+  }
+};
+
+// Adding `dot` to number
+document.getElementById("btn_dot").onclick = function () {
+  if (
+    !Array.from(document.getElementById("screen").value).includes(",") &&
+    document.getElementById("screen").value.length < 14
+  ) {
+    document.getElementById("screen").value += ",";
+  }
+};
+
+// Clear screen
+document.getElementById("btn_CE").onclick = function () {
+  document.getElementById("screen").value = "0";
+  document.getElementById("screenOperation").value = "";
+  document.getElementById("screenHistory").value = "0";
+};
+
+// Массив с идентификаторами кнопок
+const buttonOpersIds = [
+  "btn_multiply",
+  "btn_division",
+  "btn_plus",
+  "btn_minus",
+  "btn_equal",
+];
+
+buttonOpersIds.forEach((id) => {
+  document.getElementById(id).onclick = function () {
+    calcByOperator(
+      document.getElementById(id),
+      document.getElementById("screen"),
+      document.getElementById("screenHistory"),
+      document.getElementById("screenOperation")
+    );
+  };
 });
+
+// FUNCTIONS ___________________________
+
+// adding button text to screen
+function printBtnText(btn) {
+  screenText = document.getElementById("screen").value;
+  if (screenText.length < 15) {
+    if (screenText == "0") {
+      document.getElementById("screen").value = btn.value;
+    } else {
+      document.getElementById("screen").value += btn.value;
+    }
+  }
+}
+
+// Calculation by operator
+function calcByOperator(btn, screen, history, operator) {
+  if (operator.value.length == 0) {
+    history.value = screen.value;
+  } else {
+    if (operator.value == "+") {
+      history.value = String(Number(history.value) + Number(screen.value));
+    }
+    if (operator.value == "-") {
+      history.value = String(Number(history.value) - Number(screen.value));
+    }
+    if (operator.value == "*") {
+      history.value = String(Number(history.value) * Number(screen.value));
+    }
+    if (operator.value == "/") {
+      history.value = String(Number(history.value) / Number(screen.value));
+    }
+  }
+  if (btn.value == "+") {
+    operator.value = "+";
+  }
+  if (btn.value == "-") {
+    operator.value = "-";
+  }
+  if (btn.value == "*") {
+    operator.value = "*";
+  }
+  if (btn.value == "/") {
+    operator.value = "/";
+  }
+  if (btn.value == "=") {
+    operator.value = "";
+    screen.value = history.value;
+    history.value = 0;
+  }
+  if (btn.value != "=") {
+    screen.value = 0;
+  }
+}
